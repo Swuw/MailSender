fec57474bb2d747449212b100b24297adce33b78
Andrew <65603486+Swuw@users.noreply.github.com>
2023-02-12 22:25:52 +0100

    add Main controller and SendMail with config file

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendMail;
 
 class MailController extends Controller
 {
-    public function Index(){
+    public function index()
+    {
+        return view('email');
+    }
+
+    public function sendmail(Request $request)
+    {
+        $this->validate($request, [
+            'name'     =>  'required',
+            'email'  =>  'required|email',
+            'message' =>  'required'
+        ]);
+
+        $data = array(
+            'name'      =>  $request->name,
+            'message'   =>   $request->message
+        );
+
+        Mail::to($request->email)->send(new SendMail($data));
+        return back()->with('success', 'Thank you for sending message!');
 
     }
 }
